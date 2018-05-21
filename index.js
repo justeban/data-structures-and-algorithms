283fdc3a49bbb76ae0498b19b81a0ad42453448b
Justin Morris <justinjodymorris@gmail.com>
2018-05-21 03:12:15 -0700

    removed content from index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 'use strict';
 
-const LL = require('../data-structures-and-algorithms/lib/linkded-lists/linked_list.js');
-
-let list = new LL();
-
-let data = "[\"First Thing\", \"Second Thing\", \"Third Thing\", \"Fourth Thing\", \"Fifth Thing\"]";
-
-list.deserialize(data)
-console.log(list);
-
