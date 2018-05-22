8076c3d8013935ae7631f7f2532b301c7fed62d3
Justin Morris <justinjodymorris@gmail.com>
2018-05-22 16:02:50 -0700

    finished code for whiteboard

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 
 const LL = require('./lib/linkded-lists/challenge_01/ll.js');
 
+
 let list = new LL();
 
 
-list.append('222').append('333');
+list.append('222').append('333').append('444').append('555');
 
-
-list.insertBefore('333', '444');
-
-list.insertAfter('222', '555');
-console.log(list);
+list.nthFromEnd(1);
+// list.insertAfter('222', '555');
+console.log(list.nthFromEnd(-1));
