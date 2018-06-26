edf11b7cc27818e5c607e0f40401a242db52f43e
Justin Morris <justinjodymorris@gmail.com>
2018-06-26 15:03:46 -0700

    finished writing tests for the hashmap

--- __test__/lib/hashmap/hashmap.test.js
+++ __test__/lib/hashmap/hashmap.test.js
@@ -32,7 +32,7 @@ describe('HASHMAP', () => {
    
     it('expect error when key does not exist', () => {
       let hash = new Hashmap();
-      expect(hash.get('Dezi')).toBe('No Such Key');
+      expect(hash.get('Dezi')).toBeUndefined();
     });
     
     it('expect to return value if key exists', () => {
@@ -43,4 +43,63 @@ describe('HASHMAP', () => {
     });
   });
   
+  describe('delete(key', () => {
+    it('expect after run expect object at key to be undefined', () => {
+      let hash = new Hashmap();
+      hash.set('Dezi', 'Dog');
+      hash.delete('Dezi');
+      expect(hash.get('Dezi')).toBeUndefined();
+    });
+  });
+
+  describe( 'update(key, value)', () => {
+    it('when no value given expect an error', () => {
+      let hash = new Hashmap();
+      expect(() => hash.update('Dezi')).toThrow();
+    });
+
+    it('when no parameters given expected to throw an error', () => {
+      let hash = new Hashmap();
+      expect(() => hash.update()).toThrow();
+    });
+
+    it('when key given that does not match expected to throw an error', () => {
+      let hash = new Hashmap();
+      expect(() => hash.update('Dezi', 'Dog')).toThrow('');
+    });
+    it('when given valid key and value expect to update', () => {
+      let hash = new Hashmap();
+      hash.set('Dezi', 'Dog');
+      hash.update('Dezi', 'Cat');
+      expect(hash.get('Dezi')).toBe('Cat');
+    });
+  });
+
+  describe('deserializ()', () => {
+    it('when given invalid expect to throw', () => {
+      let hash = new Hashmap();
+      expect(() => hash.deserialize()).toThrow();
+    });
+
+    it('when given proper data expect to populate hashmap', () => {
+      let hash = new Hashmap();
+      hash.deserialize(`[{
+        "2087642236": {
+          "key": "Doug",
+          "value": "Dog"
+        },
+        "2087827424": {
+          "key": "Demi",
+          "value": "Cat"
+        },
+        "2087847863": {
+          "key": "Dezi",
+          "value": "Dog"
+        }
+      }]`);
+      expect(hash.get('Doug')).toBe('Dog');
+    });
+
+  });
+
 });
--- index.js
+++ index.js
@@ -10,16 +10,17 @@ let myhash = new Hashmap(0);
 myhash.set('Dezi', 'Dog');
 myhash.set('Doug', 'Dog');
 myhash.set('Demi', 'Dog');
+myhash.update('Demi', 'Cat');
 // myhash.set('Dezi');
 // console.log(util.inspect(myhash, {showHidden: true, depth: null}));
 
+// myhash.delete('Demi');
+console.log(util.inspect(myhash, { showHidden: true, depth: null }));
 
 // console.log(myhash.delete('Dezi'));
 // console.log(myhash.set('Dezi', 'Cat'));
 let data = myhash.serialize();
 
-let newHash = new Hashmap(0);
-
-newHash.deserialize(data);
+// let newHash = new Hashmap(0);
+console.log(data);
 // console.log(newHash.deserialize(data));
-console.log(util.inspect(newHash, { showHidden: true, depth: null }));
