8b81db4ce790371b9053fbf52a8198cfdbad0eeb
Justin Morris <justinjodymorris@gmail.com>
2018-06-26 08:48:46 -0700

    finished all the methods for the hashmap

--- index.js
+++ index.js
@@ -2,28 +2,24 @@
 
 const util = require('util');
 
-const KAryTree = require('./lib/trees/lib/k_ary_tree.js');
-const KAryNode = require('./lib/trees/lib/k_ary_node.js');
+const Hashmap = require('./lib/hashmap/hashmap.js');
 
-let j = new KAryNode('J');
-j.appendChild('O');
-j.appendChild('H');
-j.appendChild('N');
-
-j.children[0].appendChild('I');
-j.children[0].appendChild('S');
-
-j.children[0].children[1].appendChild('B');
-j.children[0].children[1].appendChild('A');
-j.children[0].children[1].appendChild('L');
-j.children[0].children[1].appendChild('D');
+let myhash = new Hashmap(0);
 
 
-// console.log(util.inspect(j, { showHidden: true, depth: null }));
+myhash.set('Dezi', 'Dog');
+myhash.set('Doug', 'Dog');
+myhash.set('Demi', 'Dog');
+// myhash.set('Dezi');
+// console.log(util.inspect(myhash, {showHidden: true, depth: null}));
 
-let tree = new KAryTree(j);
-let breadthTraversed = tree.breadthFirstTraversal();
-let depthTraversed = tree.depthFirstTraversal();
 
-console.log('Breadth: ', breadthTraversed);
-console.log('Depth: ', depthTraversed);
+// console.log(myhash.delete('Dezi'));
+// console.log(myhash.set('Dezi', 'Cat'));
+let data = myhash.serialize();
+
+let newHash = new Hashmap(0);
+
+newHash.deserialize(data);
+// console.log(newHash.deserialize(data));
+console.log(util.inspect(newHash, { showHidden: true, depth: null }));
