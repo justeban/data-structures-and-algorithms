4882a67e1ce194cfe9b1981d5df19e8147d2471d
Justin Morris <justinjodymorris@gmail.com>
2018-07-08 12:22:28 -0700

    finished testing on radix sort

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const radixSort = require('./lib/sorts/radix_sort.js');
 
-let arr = [3, 22, 18, 4, 1, 9, 234, 70934, 23, 1, 809];
+let arr = [3, 22, -18, 18, 4, 1, 9, 234, 70934, 23, -1, 809];
 
 console.log(radixSort(arr));
 
